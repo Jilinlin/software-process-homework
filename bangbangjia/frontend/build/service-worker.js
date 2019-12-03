33d04fae7f96961d575172f38b3600c40e22196a
hejiayao1202 <1774652466@qq.com>
2019-12-03 16:55:34 +0800

    bbj

--- bangbangjia/frontend/build/service-worker.js
+++ bangbangjia/frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d6b49aa1262bd5a83f97366f561bc6b9.js"
+  "./precache-manifest.dc2572406b52727e3243f6b201d04f77.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
